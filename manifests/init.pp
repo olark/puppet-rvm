cfb95dbca19bb2b80dc49ffcd1411b810ee8fbfd
jz <jeff.zellner@gmail.com>
2012-09-28 16:00:52 -0400

    kill this stupid stage stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,8 @@
 class rvm($version='latest', $install_rvm=true) {
-  stage { 'rvm-install': before => Stage['main'] }
-
   if $install_rvm {
     class {
-      'rvm::dependencies': stage => 'rvm-install';
-      'rvm::system':       stage => 'rvm-install', require => Package["curl"], version => $version;
+      'rvm::dependencies': ;
+      'rvm::system': version => $version, require => Package["curl"];
     }
   }
 }
